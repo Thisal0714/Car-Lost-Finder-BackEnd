ccf44bdab2793cc85a855301ba424f5ecbb4b96f
Thisal0714 <thisalchamodya123@gmail.com>
2024-12-15 23:23:04 +0530

    Commited

---
+++ src/test/java/THS/dev/demo/LostCarApplicationTests.java
@@ -0,0 +1,13 @@
+package THS.dev.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
