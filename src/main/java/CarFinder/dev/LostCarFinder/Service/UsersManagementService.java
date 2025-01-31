824cf81af76074ddb841f57636e75d695940d5ab
Thisal0714 <thisalchamodya123@gmail.com>
2025-01-31 19:26:49 +0530

    Updated

--- src/main/java/CarFinder/dev/LostCarFinder/Service/UsersManagementService.java
+++ src/main/java/CarFinder/dev/LostCarFinder/Service/UsersManagementService.java
@@ -63,7 +63,7 @@ public class UsersManagementService {
             var refreshToken = jwtUtils.generateRefreshToken(new HashMap<>(), user);
             response.setStatusCode(200);
             response.setToken(jwt);
-            response.setRole(user.getRole() );
+            response.setRole(user.getRole());
             response.setRole(user.getRole());
             response.setRefreshToken(refreshToken);
             response.setExpirationTime("24Hrs");
